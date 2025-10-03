8b535013bdc0dd929ee32b5efa64dd24bd2d4967
tholkappiyan-vst <tholkappiyan.vst@gmail.com>
2025-10-03 18:04:51 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -14,7 +14,7 @@ st.write("Upload an image and the model will predict whether it is a smile or no
 # Load training data
 # --------------------------
 smile_data = gb.glob("Dataset/Smiled/*.jpg")
-non_smile_data = gb.glob("dataset/non_smile/*.jpg")
+non_smile_data = gb.glob("Dataset/Non_smiled/*.jpg")
 
 data = []
 label = []
